f14d4d852088067e43fe8c2ae62559755d8ade1f
FranksonTeotonho <frankson.teotonho@gmail.com>
2018-10-18 10:48:16 -0300

    Tentando

--- Protocolo_TCP.py
+++ Protocolo_TCP.py
@@ -21,14 +21,24 @@ MSS = 1460 #Maximum Segment Size
 
 TESTAR_PERDA_ENVIO = False
 
-
+#Anotações
+#Send coloca na fila de envio, se fila estiver vazia já enviar. Depende da janela
+conexoes = {}
 class Conexao:
 	def __init__(self, id_conexao, seq_no, ack_no):
 		self.id_conexao = id_conexao
 		self.seq_no = seq_no
 		self.ack_no = ack_no
-		self.send_queue = b"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" + 10000 * b"Hello Pombo\n"
-conexoes = {}
+
+		self.send_base = seq_no
+		self.nextSeq_no = seq_no
+
+		self.send_queue = b""
+		self.noAck_queue = b""
+
+		self.timer = None
+
+
 
 
 #Converte endereço para string
@@ -56,13 +66,22 @@ def handle_ipv4_header(packet):
 
 	return src_addr, dst_addr, segment
 
-#Realiza Syn + ACK
+#Aceita conexão - Syn + ACK
 def make_synack(src_port, dst_port, seq_no, ack_no):
 	#Monta pacote a ser enviado
 	#(Formato dos dados, porta fonte, porta destino, Sequence Number, ACK Number,bit ACK e bit SYN, 
 	# Window Size, CheckSum, Urg data pointer)
 	return struct.pack('!HHIIHHHH', src_port, dst_port, seq_no,ack_no, (5<<12)|FLAGS_ACK|FLAGS_SYN,
                        1024, 0, 0)
+
+#Rejeita conexão - RST - Porta não disponivel
+def make_rst(src_port, dst_port, seq_no, ack_no):
+	#Monta pacote a ser enviado
+	#(Formato dos dados, porta fonte, porta destino, Sequence Number, ACK Number,bit RST, 
+	# Window Size, CheckSum, Urg data pointer)
+	return struct.pack('!HHIIHHHH', src_port, dst_port, seq_no,ack_no, (5<<12)|FLAGS_RST,
+                       1024, 0, 0)
+
 #Calcula CheckSum
 def calc_checksum(segment):
 	# se for ímpar, faz padding à direita
@@ -99,25 +118,30 @@ def fix_checksum(segment, src_addr, dst_addr):
 def send_next(fd, conexao):
 	#Obtem segmento da fila
 	payload = conexao.send_queue[:MSS]
+	
 	#Remove o segmento da fila
 	conexao.send_queue = conexao.send_queue[MSS:]
+	
 	#Obtem informações da conexão
 	(dst_addr, dst_port, src_addr, src_port) = conexao.id_conexao
+	
 	#Monta segmento para envio
 	#(Formato, porta fonte, porta destino, sequence number da conexão, ack number da conexão, 
 	# bit ACK, Window Size, CheckSum, Urg data pointer) + dados obtidos da fila de envio
 	segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
                           conexao.ack_no, (5<<12)|FLAGS_ACK,
                           1024, 0, 0) + payload
+	
 	#Geração do novo sequence number
 	conexao.seq_no = (conexao.seq_no + len(payload)) & 0xffffffff
+	
 	#Segmento com checksum
 	segment = fix_checksum(segment, src_addr, dst_addr)
 
 	#
 	if not TESTAR_PERDA_ENVIO or random.random() < 0.95:
 		fd.sendto(segment, (dst_addr, dst_port))
-	#
+	#Finaliza conexão
 	if conexao.send_queue == b"":
 		segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
                           conexao.ack_no, (5<<12)|FLAGS_FIN|FLAGS_ACK,
@@ -128,44 +152,70 @@ def send_next(fd, conexao):
 	else:
 		asyncio.get_event_loop().call_later(.001, send_next, fd, conexao)
 
-#Recebe
+
+def ack_recv():
+	return
+
+def payload_recv():
+	return
+
+def send(fd, conexao, payload):
+	conexao.send_queue += payload 
+	return
+
+
+#Recebe novos dados do raw socket
 def raw_recv(fd):
 	#Recebe um pacote do socket
 	packet = fd.recv(12000)
+	
 	#Tratamento do cabeçalho da camada de rede
 	src_addr, dst_addr, segment = handle_ipv4_header(packet)
+	
 	#Recupera informações do pack
 	#(Formato, porta fonte, porta destino, sequence number da conexão, ack number da conexão, 
 	# bit ACK, Window Size, CheckSum, Urg data pointer)
 	src_port, dst_port, seq_no, ack_no, \
         	flags, window_size, checksum, urg_ptr = \
         	struct.unpack('!HHIIHHHH', segment[:20])
+	
 	#identificador da conexão
 	id_conexao = (src_addr, src_port, dst_addr, dst_port)
 	
 	#Aceita somente a porta 7000
 	if dst_port != 7000:
 		return
-	
+		#segment = struct.pack('!HHIIHHHH', src_port, dst_port, struct.unpack('I', os.urandom(4))[0] ,
+        #                  seq_no + 1, (5<<12)|FLAGS_RST,
+        #                  0, 0, 0)
+		#segment = fix_checksum(segment, src_addr, dst_addr)
+		#fd.sendto(segment, (dst_addr, dst_port))
+		
 	#
+	#print(segment.decode())
 	payload = segment[4*(flags>>12):]
-	print(payload.decode())
 	#Identificação das flags
+	#Conexão requerida e aceita
 	if (flags & FLAGS_SYN) == FLAGS_SYN:
 		print('%s:%d -> %s:%d (seq=%d)' % (src_addr, src_port,
                                            dst_addr, dst_port, seq_no))
 
+		#Alocando nova conexão
 		conexoes[id_conexao] = conexao = Conexao(id_conexao=id_conexao,
-                                                 seq_no=struct.unpack('I', os.urandom(4))[0],
-                                                 ack_no=seq_no + 1)
-
+	                                                 seq_no=struct.unpack('I', os.urandom(4))[0],
+	                                                 ack_no=seq_no + 1)
+	
 		fd.sendto(fix_checksum(make_synack(dst_port, src_port, conexao.seq_no, conexao.ack_no),
-                               src_addr, dst_addr),
-                  (src_addr, src_port))
-
+	                           src_addr, dst_addr),
+	              (src_addr, src_port))
+			
+		print("Seq_no: %d \n", conexao.seq_no)
+		print("Ack_no: %d \n",conexao.ack_no)
 		conexao.seq_no += 1
 
-		asyncio.get_event_loop().call_later(.1, send_next, fd, conexao)
+		#asyncio.get_event_loop().call_later(.1, send_next, fd, conexao)
+		
+	
 	#
 	elif id_conexao in conexoes:
 		conexao = conexoes[id_conexao]
